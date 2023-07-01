d81bb178c04c10800ba470865bdd16ab95ffafb8
Ivan Gorbunov <m1al04949@gmail.com>
2023-07-01 18:14:55 +0300

    Init Logger And Sqlite, but CGO_ENABLED=0 failed

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/m1al04949/url-shortener/internal/config"
+	"github.com/m1al04949/url-shortener/internal/lib/logger/logslog"
+	"github.com/m1al04949/url-shortener/internal/storage/sqlite"
+	"golang.org/x/exp/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -11,11 +21,41 @@ func main() {
 
 	fmt.Println(cfg)
 
-	//TODO: init logger: slog
+	log := setupLogger(cfg.Env)
 
-	//TODO: init storage: sqlite
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", logslog.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	//TODO: init router: chi, "chi render"
 
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
