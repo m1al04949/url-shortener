80320e69fbb1787617c0b7d2108456efbcb469c2
Ivan Gorbunov <m1al04949@gmail.com>
2023-07-02 16:20:51 +0300

    Init Router, Add Pretty Slog

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/m1al04949/url-shortener/internal/config"
+	mwLogger "github.com/m1al04949/url-shortener/internal/http-server/middleware/logger"
+	"github.com/m1al04949/url-shortener/internal/lib/logger/handlers/slogpretty"
 	"github.com/m1al04949/url-shortener/internal/lib/logger/logslog"
 	"github.com/m1al04949/url-shortener/internal/storage/sqlite"
 	"golang.org/x/exp/slog"
@@ -25,6 +29,7 @@ func main() {
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+	log.Error("error messages are enabled")
 
 	storage, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
@@ -34,7 +39,14 @@ func main() {
 
 	_ = storage
 
-	//TODO: init router: chi, "chi render"
+	//TODO: "chi render"
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	//TODO: logger requests: router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 
 	//TODO: run server
 }
@@ -44,9 +56,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -58,4 +68,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
